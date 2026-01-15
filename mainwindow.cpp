ea874bf93bf72edde838516e37a088b3c8e5628d
Evan <a1263221841@gmail.com>
2026-01-15 16:09:44 +0800

    1.15laptop(2)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,25 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //窗口大小尺寸设计
+    setFixedSize(width(),height());
+
+    //多行文本编辑控件设置
+    ui->plainTextEdit_MsgList->setReadOnly(true);
+    ui->plainTextEdit_SendMsg->setFocus();
+
+    //默认发送文本测试文件
+    ui->plainTextEdit_SendMsg->setPlainText("Hello TCP Server/Client");
+
+    //UI命令按扭设置
+    ui->pushButton_SendMsg->setEnabled(false);
+    ui->pushButton_UdpTest->setEnabled(false);
+    ui->pushButton_ConnectServer->setEnabled(false);
+    ui->pushButton_DisconnectServer->setEnabled(false);
+    ui->pushButton_CloseListen->setEnabled(false);
+    ui->plainTextEdit_SendMsg->setEnabled(false);
+    ui->checkBox_ClientAutTest->setEnabled(false);
+    ui->radioButton_Server->setEnabled(true);
 }
 
 MainWindow::~MainWindow()
